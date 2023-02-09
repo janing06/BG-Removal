8e268d2a050cff04f7a75573cf447700edeb16a3
janing06 <120067518+janing06@users.noreply.github.com>
2023-02-09 09:57:49 +0800

    up

--- mydjango/settings.py
+++ mydjango/settings.py
@@ -136,7 +136,6 @@ STATIC_URL = '/static/'
 #MEDIA_URL = '/media/'
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
-STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
